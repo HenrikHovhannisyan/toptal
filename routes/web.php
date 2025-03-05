0bd080ea93d9b609dee2d0b68af6dbb063ed3fec
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-03-05 09:59:49 +0400

    Change Laravel version

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use App\Http\Controllers\SendMailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', [PagesController::class, 'index'])->name('home');
 Route::get('/product', [PagesController::class, 'product'])->name('product');
 Route::get('/resources', [PagesController::class, 'resources'])->name('resources');
@@ -11,4 +22,3 @@ Route::get('/terms-condition', [PagesController::class, 'termsCondition'])->name
 Route::get('/privacy-policy', [PagesController::class, 'privacyPolicy'])->name('privacy-policy');
 
 Route::post('/send-mail', [SendMailController::class, 'sendMail'])->name('send.mail');
-
