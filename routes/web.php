b7b872f95da7fe0103bded467d1564fa9fe4e0fb
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-04-08 12:32:50 +0400

    Changed link

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\PagesController;
 
 Route::get('/', [PagesController::class, 'index'])->name('home');
 Route::get('/product', [PagesController::class, 'product'])->name('product');
-Route::get('/resources', [PagesController::class, 'resources'])->name('resources');
+Route::get('/resource', [PagesController::class, 'resources'])->name('resources');
 Route::get('/terms-condition', [PagesController::class, 'termsCondition'])->name('terms-condition');
 Route::get('/privacy-policy', [PagesController::class, 'privacyPolicy'])->name('privacy-policy');
 
